4ca7a696cd1fcc222294a413ec524880655ab167
Ryan Patterson <ry@fb.com>
2012-04-07 14:14:14 -0700

    make mock non-global

--- public/js/mock.js
+++ public/js/mock.js
@@ -1,38 +1,4 @@
-(function() {
-  const PLAYER_DB = [
-    {
-      id: 693594821,
-      name: 'Ryan',
-      full_name: 'Ryan Patterson',
-      profile_pic: 'http://profile.ak.fbcdn.net/hprofile-ak-snc4/273581_693594821_24739666_q.jpg'
-    }, {
-      id: 1341660327,
-      name: 'Zizhuang',
-      full_name: 'Zizhuang Yang',
-      profile_pic: 'http://profile.ak.fbcdn.net/hprofile-ak-snc4/275664_1341660327_177798061_q.jpg'
-    }, {
-      id: 1599450468,
-      name: 'Alex',
-      full_name: 'Alex Wyler',
-      profile_pic: 'http://profile.ak.fbcdn.net/hprofile-ak-snc4/174184_1599450468_1761600976_q.jpg',
-    }, {
-      id: 571997087,
-      name: 'Mary',
-      full_name: 'Mary Pimenova',
-      profile_pic: 'http://profile.ak.fbcdn.net/hprofile-ak-ash2/565181_571997087_2000265899_q.jpg'
-    }, {
-      id: 6203644,
-      name: 'Derek',
-      full_name: 'Derek Brandao',
-      profile_pic: 'http://profile.ak.fbcdn.net/hprofile-ak-snc4/174156_6203644_1590218817_q.jpg'
-    }, {
-      id: 1653295587,
-      name: 'Ben',
-      full_name: 'Ben Zax',
-      profile_pic: 'http://profile.ak.fbcdn.net/hprofile-ak-snc4/41760_1653295587_4599_q.jpg'
-    }
-  ];
-
+function Mock() {
   socket = {
     emit: function(event, data) {
       this[event] && this[event].call(this, data);
@@ -48,7 +14,7 @@
   var game = new Game(), mission;
 
   clientstate.didJoinGame(game);
-  game.players.add(PLAYER_DB, { parse: true });
+  game.players.add(Mock.PLAYER_DB, { parse: true });
   mission = new Mission({
     turn: 1,
     attempt: 1,
@@ -90,4 +56,38 @@
     var func = mock_steps.shift();
     func();
   };
-})();
+}
+
+Mock.PLAYER_DB = [
+  {
+    id: 693594821,
+    name: 'Ryan',
+    full_name: 'Ryan Patterson',
+    profile_pic: 'http://profile.ak.fbcdn.net/hprofile-ak-snc4/273581_693594821_24739666_q.jpg'
+  }, {
+    id: 1341660327,
+    name: 'Zizhuang',
+    full_name: 'Zizhuang Yang',
+    profile_pic: 'http://profile.ak.fbcdn.net/hprofile-ak-snc4/275664_1341660327_177798061_q.jpg'
+  }, {
+    id: 1599450468,
+    name: 'Alex',
+    full_name: 'Alex Wyler',
+    profile_pic: 'http://profile.ak.fbcdn.net/hprofile-ak-snc4/174184_1599450468_1761600976_q.jpg',
+  }, {
+    id: 571997087,
+    name: 'Mary',
+    full_name: 'Mary Pimenova',
+    profile_pic: 'http://profile.ak.fbcdn.net/hprofile-ak-ash2/565181_571997087_2000265899_q.jpg'
+  }, {
+    id: 6203644,
+    name: 'Derek',
+    full_name: 'Derek Brandao',
+    profile_pic: 'http://profile.ak.fbcdn.net/hprofile-ak-snc4/174156_6203644_1590218817_q.jpg'
+  }, {
+    id: 1653295587,
+    name: 'Ben',
+    full_name: 'Ben Zax',
+    profile_pic: 'http://profile.ak.fbcdn.net/hprofile-ak-snc4/41760_1653295587_4599_q.jpg'
+  }
+];
