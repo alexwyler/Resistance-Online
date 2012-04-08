306da610d8bf431469569c53341362a80721a67b
Alex Wyler <alex.s.wyler@gmail.com>
2012-04-08 05:05:07 -0500

    bare minimum 'reload' button on error screen :D

--- public/js/views/ErrorView.js
+++ public/js/views/ErrorView.js
@@ -6,6 +6,7 @@ exports.ErrorView = Backbone.View.extend({
       '<div id="error_view" class="viewport center">',
         '<div class="error_state center title layer">Error</div>',
         '<div class="title">{{error}}</div>',
+        '<input type="button" onclick="window.location.reload()" value="Reload"/>',
       '</div>'
     ].join('');
     this.$el.html(Mustache.render(template, this.options));
