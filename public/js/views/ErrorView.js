f21f1e1949059f1736e93ba4371ff1fe6f901a9c
Ryan Patterson <ry@fb.com>
2012-04-08 03:33:56 -0700

    minor css tweaks to non-game sutff

--- public/js/views/ErrorView.js
+++ public/js/views/ErrorView.js
@@ -3,11 +3,9 @@ var Backbone = require('backbone');
 exports.ErrorView = Backbone.View.extend({
   render: function() {
     var template = [
-      '<div id="error_view" class="viewport center">',
-        '<div class="error_state center title layer">Error</div>',
-        '<div class="title">{{error}}</div>',
-        '<input type="button" onclick="window.location.reload()" value="Reload"/>',
-      '</div>'
+      '<div class="error_state center title layer">Error</div>',
+      '<div class="title">{{error}}</div>',
+      '<input type="button" onclick="window.location.reload()" value="Reload"/>'
     ].join('');
     this.$el.html(Mustache.render(template, this.options));
     return this;
--- public/js/views/LoginView.js
+++ public/js/views/LoginView.js
@@ -3,12 +3,10 @@ var Backbone = require('backbone');
 exports.LoginView = Backbone.View.extend({
   render: function() {
     this.$el.html(
-      '<div id="login_page" class="viewport center">' +
         '<br/>  <br/>  <br/>' +
         '<h1 class="center">The Resistance</h1>' +
         '<br/>  <br/>  <br/>' +
-        '<div class="fb-login-button center"></div>' +
-      '</div>');
+        '<div class="fb-login-button center"></div>');
     return this;
   }
 });
